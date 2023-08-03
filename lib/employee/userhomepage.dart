5289da034d1683a3ed25eedaab6a2e12c71b4a58
santhikc12@gmail.com <shanfshan223@gmail.com>
2023-08-03 14:31:50 +0530

    employee

--- lib/employee/userhomepage.dart
+++ lib/employee/userhomepage.dart
@@ -8,7 +8,7 @@ class UserHomePage extends StatefulWidget {
 }
 
 class _UserHomePageState extends State<UserHomePage> {
-  List<String>workname=['work details','pending work','approved work','completed work','cancelled work'];
+  List<String>workname=['pending work','approved work','completed work','cancelled work'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +43,7 @@ class _UserHomePageState extends State<UserHomePage> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           CircleAvatar(
-                            radius: 30,
+                            radius: 50,
                             backgroundImage:
                             AssetImage('assetss/profilepic.jpg'),
                           ),
@@ -91,37 +91,9 @@ class _UserHomePageState extends State<UserHomePage> {
                         ],
                       ),
                     ),
-                    SizedBox(
-                      height: 12,
-                    ),
 
-                    Container(
-                      margin: EdgeInsets.only(top: 15, bottom: 20),
-                      width: MediaQuery.of(context).size.width,
-                      height: 60,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(25),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black12,
-                            )
-                          ]),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Search here...",
-                          hintStyle: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                          ),
-                          prefixIcon: Icon(
-                            Icons.search,
-                            size: 25,
-                          ),
-                        ),
-                      ),
-                    ),
+
+
                   ],
                 ),
               ),
@@ -164,7 +136,7 @@ class _UserHomePageState extends State<UserHomePage> {
                         margin: EdgeInsets.symmetric(
                             vertical: 5, horizontal:15),
                         height: 100,
-                        width: 130,
+                        width: 140,
                         decoration: BoxDecoration(
                             color: Colors.white,
                             border: Border.all(color: Colors.black12),
@@ -191,9 +163,7 @@ class _UserHomePageState extends State<UserHomePage> {
                         ) ,
 
                       ),
-                      SizedBox(
-                        height: 15,
-                      ),
+
 
                     ],
                   );
@@ -204,170 +174,7 @@ class _UserHomePageState extends State<UserHomePage> {
               height: 30,
             ),
 
-            Padding(padding: EdgeInsets.only(left:15),
-                child:Text("Working Hours", style:TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w800,
-                  color: Colors.black.withOpacity(0.7),
-                ))),
-      SizedBox(
-        height: 30,
-      ),
-      Padding(
-        padding: const EdgeInsets.all(10),
-        child: Container(
-          margin: EdgeInsets.symmetric(
-              vertical: 5, horizontal:15),
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: Colors.black12),
-            boxShadow: [BoxShadow(
-              color: Colors.black38,
-              blurRadius: 4,
-              spreadRadius: 2,
-            ),
-            ],
 
-          ),
-          child:Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Center(
-                  child: Text("Morning Time",
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.black.withOpacity(0.7),
-
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Text("8.00 AM to 1.00 PM",
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.black.withOpacity(0.7),
-
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ) ,
-
-        ),
-      ),
-      SizedBox(
-        height: 15,
-      ),
-
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Container(
-                margin: EdgeInsets.symmetric(
-                    vertical: 5, horizontal:15),
-                height: 100,
-                width: 90,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black12),
-                  boxShadow: [BoxShadow(
-                    color: Colors.black38,
-                    blurRadius: 4,
-                    spreadRadius: 2,
-                  ),
-                  ],
-
-                ),
-                child:Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      Center(
-                        child: Text("Lunch Time",
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.w900,
-                            color: Colors.black.withOpacity(0.7),
-
-                          ),
-                        ),
-                      ),
-                      Center(
-                        child: Text("1.00 AM to 2.00 PM",
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.w900,
-                            color: Colors.black.withOpacity(0.7),
-
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ) ,
-
-              ),
-            ),
-
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Container(
-                margin: EdgeInsets.symmetric(
-                    vertical: 5, horizontal:15),
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black12),
-                  boxShadow: [BoxShadow(
-                    color: Colors.black38,
-                    blurRadius: 4,
-                    spreadRadius: 2,
-                  ),
-                  ],
-
-                ),
-                child:Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      Center(
-                        child: Text("Evening Time",
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.w900,
-                            color: Colors.black.withOpacity(0.7),
-
-                          ),
-                        ),
-                      ),
-                      Center(
-                        child: Text("2.00 AM to 5.00 PM",
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.w900,
-                            color: Colors.black.withOpacity(0.7),
-
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ) ,
-
-              ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
 
 
 
